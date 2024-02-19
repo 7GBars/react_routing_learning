8263243447ad3b2d4881cda8c8df83cfc35ff2c9
Barsegyan <barsegyan@topsystems.ru>
2024-02-19 18:27:41 +0300

    -init commit

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} /> a
+    <RouterProvider router={router} /> aa
   </React.StrictMode>
 );
