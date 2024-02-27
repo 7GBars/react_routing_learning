97ecf1922a9e07921522b637c38d121f3f523dbc
Barsegyan <barsegyan@topsystems.ru>
2024-02-27 20:09:58 +0300

    -добавить и проматить карточки

--- src/routes/UX.tsx
+++ src/routes/UX.tsx
@@ -1,7 +1,17 @@
+import {useState} from "react";
+
 export function UX() {
+  const [cards, setCards] = useState([
+    {id: 1, order: 1, title: 'Карточка 1'},
+    {id: 2, order: 2, title: 'Карточка 2'},
+    {id: 2, order: 2, title: 'Карточка 2'},
+  ]);
 
   return (
-    <div></div>
+    <div>
+      {cards.map(c => <div>{c.title}</div>)}
+      <button onClick={() => setCards(cards)}>Перемешать</button>
+    </div>
   );
 }
 
